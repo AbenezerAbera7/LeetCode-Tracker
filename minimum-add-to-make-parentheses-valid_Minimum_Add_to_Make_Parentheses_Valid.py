cfaf85a61a73c0205d98efa2709cbb4697433fef
Abenezer Abera <abenezer.aberaa@gmail.com>
2024-10-22 15:10:00 +0300

    Add Minimum Add to Make Parentheses Valid

---
+++ minimum-add-to-make-parentheses-valid_Minimum_Add_to_Make_Parentheses_Valid.py
@@ -0,0 +1,15 @@
+class Solution:
+    def minAddToMakeValid(self, s: str) -> int:
+
+        stack = []
+        res = 0
+        for ch in s:
+            if ch == "(":
+                stack.append(ch)
+            else:
+                if stack:
+                    stack.pop()
+                else:
+                    res += 1
+        return len(stack) + res
+        
