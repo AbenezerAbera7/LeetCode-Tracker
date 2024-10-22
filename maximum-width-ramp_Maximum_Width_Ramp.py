5357557e456cb7edfbf60ba19fe24b1b09f8799d
Abenezer Abera <abenezer.aberaa@gmail.com>
2024-10-22 15:36:50 +0300

    Add Maximum Width Ramp

--- maximum-width-ramp_Maximum_Width_Ramp.py
+++ maximum-width-ramp_Maximum_Width_Ramp.py
@@ -1,10 +1,18 @@
 class Solution:
     def maxWidthRamp(self, nums: List[int]) -> int:
+        n = len(nums)
+        indices = [i for i in range(n)]
+        print(indices)
 
-        maxu = 0
-        for i in range(len(nums)-1):
-            for j in range(i+1, len(nums)):
-                if nums[i] <= nums[j]:
-                    maxu = max(maxu, j - i)
-        return maxu
-        
+        indices.sort(key=lambda i: (nums[i], i))
+        print(indices)
+
+        min_index = n  
+        max_width = 0
+
+
+        for i in indices:
+            max_width = max(max_width, i - min_index)
+            min_index = min(min_index, i)
+
+        return max_width
