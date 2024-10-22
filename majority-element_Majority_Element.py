ab4a273b99d83de897af2c50e29be7bf16f21965
Abenezer Abera <abenezer.aberaa@gmail.com>
2024-10-22 15:36:14 +0300

    Add Majority Element

--- majority-element_Majority_Element.py
+++ majority-element_Majority_Element.py
@@ -2,7 +2,7 @@ class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         
         dictu = collections.Counter(nums)
-        n = len(nums)
+        n = len(dictu)
 
         for key, value in dictu.items():
             if value > n / 2:
